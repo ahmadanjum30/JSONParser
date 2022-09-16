347bee328d4191f74101f9da29d4176b439bd4da
unknown <ahmad.anjum@devsinc.com>
2022-09-16 18:16:17 +0500

    Added Pagination

--- src/Components/MainView.jsx
+++ src/Components/MainView.jsx
@@ -2,15 +2,18 @@ import './styles.css'
 
 import CarInfo from './CarInfo'
 import EditForm from './EditForm'
+import Protected from './Protected'
 
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { deleteCar, requestCars } from './Redux/action'
 import { Button, Card, Container } from '@mui/material'
 import { VerifiedUser, Delete, Edit } from '@mui/icons-material'
-import Protected from './Protected'
+import { Box } from '@mui/system'
+import { ArrowForwardIos, ArrowBackIosNew } from '@mui/icons-material'
 
 const MainView = () => {
+  const [currentPage, setCurrentPage] = useState(1)
   const [user, setUser] = useState(3)
   const dispatch = useDispatch()
   const { carsData, isLoading } = useSelector((state) => state)
@@ -33,63 +36,91 @@ const MainView = () => {
     setUser(((user + 1) % 5) + 1)
   }
 
+  const handlePagination = (isNext) => {
+    if (isNext) {
+      setCurrentPage(currentPage + 1)
+    } else {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+
   return (
     <>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Button
+          onClick={() => {
+            handlePagination(false)
+          }}
+          startIcon={<ArrowBackIosNew />}
+          disabled={currentPage <= 1}>
+          Back
+        </Button>
+        <Button
+          onClick={() => {
+            handlePagination(true)
+          }}
+          endIcon={<ArrowForwardIos />}
+          disabled={currentPage * 10 >= carsData.length}>
+          Next
+        </Button>
+      </Box>
       {isLoading && <div className="loading">Data loading...</div>}
-      {carsData.map((cars) => {
+      {carsData.map((cars, index) => {
         return (
           <Container key={cars.id}>
-            <Card sx={{ p: 4, mt: 2, mb: 2, ml: 10, mr: 10, boxShadow: 3 }} variant="outlined">
-              <div className="row">
-                <div className="col-md-4 border-end border-info">
-                  <img src={`${cars.image}`} />
-                </div>
-                {showForm === cars.id ? (
-                  <div className="col-md-4">
-                    <EditForm cars={cars} />
+            {index >= (currentPage - 1) * 10 && index < currentPage * 10 && (
+              <Card sx={{ p: 4, mt: 2, mb: 2, ml: 10, mr: 10, boxShadow: 3 }} variant="outlined">
+                <div className="row">
+                  <div className="col-md-4 border-end border-info">
+                    <img src={`${cars.image}`} />
                   </div>
-                ) : (
-                  <CarInfo cars={cars} />
-                )}
-                <div className="col-md-4 mt-5 p-4">
-                  <h2 className="price-car">
-                    <b>PKR {cars.price}</b>
-                  </h2>
+                  {showForm === cars.id ? (
+                    <div className="col-md-4">
+                      <EditForm cars={cars} />
+                    </div>
+                  ) : (
+                    <CarInfo cars={cars} />
+                  )}
+                  <div className="col-md-4 mt-5 p-4">
+                    <h2 className="price-car">
+                      <b>PKR {cars.price}</b>
+                    </h2>
 
-                  <Protected isLoggedIn={user === cars.user}>
+                    <Protected isLoggedIn={user === cars.user}>
+                      <Button
+                        onClick={() => {
+                          handleEditClick(cars)
+                        }}
+                        className="btn btn-primary"
+                        type="submit"
+                        endIcon={<Edit />}>
+                        {showForm === cars.id ? 'Back' : 'Edit'}
+                      </Button>
+                      <Button
+                        onClick={() => {
+                          onDelete(cars.id)
+                        }}
+                        color="error"
+                        variant="outlined"
+                        startIcon={<Delete />}>
+                        Delete
+                      </Button>
+                    </Protected>
+
                     <Button
                       onClick={() => {
-                        handleEditClick(cars)
+                        addUser()
                       }}
-                      className="btn btn-primary"
-                      type="submit"
-                      endIcon={<Edit />}>
-                      {showForm === cars.id ? 'Back' : 'Edit'}
-                    </Button>
-                    <Button
-                      onClick={() => {
-                        onDelete(cars.id)
-                      }}
-                      color="error"
+                      sx={{ m: 2 }}
+                      color="success"
                       variant="outlined"
-                      startIcon={<Delete />}>
-                      Delete
+                      startIcon={<VerifiedUser />}>
+                      User+1
                     </Button>
-                  </Protected>
-
-                  <Button
-                    onClick={() => {
-                      addUser()
-                    }}
-                    sx={{ m: 2 }}
-                    color="success"
-                    variant="outlined"
-                    startIcon={<VerifiedUser />}>
-                    User+1
-                  </Button>
+                  </div>
                 </div>
-              </div>
-            </Card>
+              </Card>
+            )}
           </Container>
         )
       })}
