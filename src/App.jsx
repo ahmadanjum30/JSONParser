556ddf2c59d86f885c4baa29cb72bcfb2c6165dc
unknown <ahmad.anjum@devsinc.com>
2022-09-27 17:43:19 +0500

    Heroku Deployment Removed Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import './App.css'
 import { store } from './Components/Redux/store'
 import { Provider } from 'react-redux'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import MainView from './Components/MainView'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Provider store={store}>
-              <MainView />
-            </Provider>
-          }></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <MainView />
+    </Provider>
   )
 }
 
