7c09090007ece4f65e64b76c97a0ee3c632ec1a0
unknown <ahmad.anjum@devsinc.com>
2022-09-27 17:49:26 +0500

    Readded Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css'
 import { store } from './Components/Redux/store'
 import { Provider } from 'react-redux'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import MainView from './Components/MainView'
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <MainView />
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Provider store={store}>
+              <MainView />
+            </Provider>
+          }></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
